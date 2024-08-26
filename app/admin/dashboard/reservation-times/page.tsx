a7966e44d13215c80a32792f05f220b3720bb127
Pavle Jovanovic <pavle.jovanovic@source.ag>
2024-08-26 16:11:37 +0200

    adding missing pages

---
+++ app/admin/dashboard/reservation-times/page.tsx
@@ -0,0 +1,3 @@
+export default function ReservationTimesPage() {
+    return <>Definiši termine za rezervaciju</>;
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
-export default function PricesPage() {
+export default function ContactPage() {
     return (
         <section className="flex h-screen w-full flex-col items-center justify-center text-2xl">
-            <h1>Stranica sa cenama</h1>
+            <h1>Stranica za kontakt</h1>
         </section>
     );
 }
