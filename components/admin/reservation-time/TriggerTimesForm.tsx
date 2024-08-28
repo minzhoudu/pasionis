1f4029c81b58a4e2ccfab196d83c25522e7740f7
Pavle Jovanovic <pavle.jovanovic@source.ag>
2024-08-28 12:11:09 +0200

    responsive times

--- components/admin/reservation-time/TriggerTimesForm.tsx
+++ components/admin/reservation-time/TriggerTimesForm.tsx
@@ -8,12 +8,12 @@ type TriggerTimesFormProps = {
 
 export const TriggerTimesForm = ({ times }: TriggerTimesFormProps) => {
     return (
-        <div className="max-h-52 overflow-y-scroll rounded-md border p-5">
+        <div className="max-h-52 overflow-y-scroll rounded-md border p-2 md:p-5">
             <form className="flex flex-wrap justify-center gap-7">
                 {times.map((reservationTime) => (
                     <button
                         key={reservationTime._id.toString()}
-                        className={`w-16 rounded-md border px-2 py-1 text-center font-bold ${!reservationTime.available ? "border-red-500 bg-red-500 opacity-50" : ""}`}
+                        className={`w-16 rounded-md border px-2 py-1 text-center text-sm font-bold lg:text-base ${!reservationTime.available ? "border-red-500 bg-red-500 opacity-50" : ""}`}
                         formAction={triggerReservationTime.bind(
                             null,
                             reservationTime._id.toString(),
