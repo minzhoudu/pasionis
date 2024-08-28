25f5483b7fae565dfb58077392e0bc99f6ab099c
Pavle Jovanovic <pavle.jovanovic@source.ag>
2024-08-28 09:33:24 +0200

    loading fix

--- app/admin/dashboard/loading.tsx
+++ app/admin/dashboard/loading.tsx
@@ -1,6 +1,6 @@
 import { CgSpinnerTwoAlt } from "react-icons/cg";
 
-export default function MainLoadingPage() {
+export default function DashboardLoadingPage() {
     return (
         <div className="flex h-screen w-full animate-spin flex-col items-center justify-center">
             <CgSpinnerTwoAlt size={60} />
--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -1,4 +1,4 @@
-export default function DashboardLoadingPage() {
+export default function AdminLoadingPage() {
     return (
         <div className="flex h-screen w-full animate-pulse flex-col items-center justify-center">
             <h1 className="text-2xl">Učitavanje admin panela...</h1>
